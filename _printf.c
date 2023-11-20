53e8bbbe67e8f7adc145662f5c665f16d556122a
Omar J <omarj819@gmail.com>
2023-11-21 01:54:53 +0200

    A

--- _printf.c
+++ _printf.c
@@ -2,7 +2,23 @@
 
 int _printf(const char *format, ...)
 {
-    /* Implementation of _printf function */
-    /* You'll need to use va_start, va_arg, va_copy, and va_end for variable arguments */
-    /* Use write function to print to stdout */
+    int printed_chars = 0;
+    va_list args;
+
+    (void)format; /* To silence the unused parameter warning */
+
+    /* Start processing variable arguments */
+    va_start(args, format);
+
+    /* TODO: Implement logic for handling format specifiers and printing */
+    /* Example: printing a string */
+    const char *str = "Hello, world!";
+    int str_len = 13; /* The length of the string */
+
+    printed_chars = write(1, str, str_len); /* Assuming write is used to print to stdout */
+
+    /* End processing variable arguments */
+    va_end(args);
+
+    return printed_chars;
 }

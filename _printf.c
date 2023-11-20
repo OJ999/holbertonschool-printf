2aaab6332a795f5fb5bf64795fad640f80611777
Omar J <omarj819@gmail.com>
2023-11-21 01:57:36 +0200

    A

--- _printf.c
+++ _printf.c
@@ -3,17 +3,17 @@
 
 int _printf(const char *format, ...)
 {
-    int printed_chars = 0;
     va_list args;
+    int printed_chars = 0;
 
     (void)format; /* To silence the unused parameter warning */
+
+    /* Start processing variable arguments */
+    va_start(args, format);
 
     /* Example: printing a string */
     const char *str = "Hello, world!";
     int str_len = 13; /* The length of the string */
-
-    /* Start processing variable arguments */
-    va_start(args, format);
 
     printed_chars = write(1, str, str_len); /* Assuming write is used to print to stdout */
 

ea0efc521c573e27709a3d687c8bc60352fff87d
Omar J <omarj819@gmail.com>
2023-11-21 02:14:46 +0200

    A

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
-#include <string.h> // Include for strlen function
+#include <string.h> /* Include for strlen function */
 
 /**
  * _printf - Custom printf function
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
     if (!format)
     {
         write(1, "(null)", 6);
-        return -1; // Handle NULL format
+        return -1; /* Handle NULL format */
     }
 
     va_list args;
@@ -26,14 +26,14 @@ int _printf(const char *format, ...)
     {
         if (*str == '%' && *(str + 1) != '\0')
         {
-            str++; // Move past '%'
+            str++; /* Move past '%' */
             char c = *str;
 
             switch (c)
             {
                 case 'c':
                     {
-                        char char_arg = va_arg(args, int); // Cast to char
+                        char char_arg = va_arg(args, int); /* Cast to char */
                         printed_chars += write(1, &char_arg, 1);
                     }
                     break;

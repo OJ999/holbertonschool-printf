7074fe71672bcddb88d4cc067bbc4feecc347b91
Omar J <omarj819@gmail.com>
2023-11-21 02:01:51 +0200

    A

--- _printf.c
+++ _printf.c
@@ -10,21 +10,19 @@ int _printf(const char *format, ...)
 
     (void)format; /* To silence the unused parameter warning */
 
-    /* Example: printing a string */
-    const char *str;
-    int str_len;
-
     /* Start processing variable arguments */
     va_start(args, format);
 
-    /* Assign values to variables after variable declarations */
-    str = "Hello, world!";
-    str_len = 13; /* The length of the string */
+    /* Example: printing a string */
+    {
+        const char *str = "Hello, world!";
+        int str_len = 13; /* The length of the string */
 
-    /* End processing variable arguments */
-    va_end(args);
+        /* End processing variable arguments */
+        va_end(args);
 
-    printed_chars = write(1, str, str_len); /* Assuming write is used to print to stdout */
+        printed_chars = write(1, str, str_len); /* Assuming write is used to print to stdout */
+    }
 
     return printed_chars;
 }

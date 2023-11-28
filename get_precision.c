a133342c25ecabc6855acb9b1e4f16e1c3c5de7f
Omar J <omarj819@gmail.com>
2023-11-29 00:59:25 +0200

    A

--- get_precision.c
+++ get_precision.c
@@ -3,8 +3,8 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Index in the format string
+ * @list: List of arguments
  *
  * Return: Precision.
  */
@@ -45,4 +45,27 @@ int get_precision(const char *format, int *i, va_list list)
     *i = curr_i - 1;
 
     return (precision);
+}
+
+/**
+ * print_string - Print a string to the buffer with precision
+ * @buffer: Buffer to print to
+ * @buff_ind: Index at which to add next char, represents the length.
+ * @str: String to print
+ * @precision: Maximum number of characters to print
+ * Return: Number of characters printed
+ */
+int print_string(char buffer[], int *buff_ind, const char *str, int precision)
+{
+    int i;
+
+    for (i = 0; i < precision && str[i] != '\0'; i++)
+    {
+        buffer[*buff_ind] = str[i];
+        (*buff_ind)++;
+        if (*buff_ind == BUFF_SIZE)
+            print_buffer(buffer, buff_ind);
+    }
+
+    return i; // Return the number of characters printed
 }

5ac8a9c1dcf030e82363e8546111b162987b4de7
Omar J <omarj819@gmail.com>
2023-11-29 01:10:35 +0200

    A

--- get_precision.c
+++ get_precision.c
@@ -3,40 +3,46 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Index in the format string
- * @list: List of arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-    (void)format; // Unused parameter
-    (void)i;      // Unused parameter
-    (void)list;   // Unused parameter
-
+    int curr_i = *i + 1;
     int precision = -1;
 
-    // Your existing implementation for getting precision
+    if (format[curr_i] != '.')
+        return (precision);
 
-    return precision;
-}
+    precision = 0;
 
-/**
- * print_string - Print a string to the buffer with precision
- * @buffer: Buffer to print to
- * @buff_ind: Index at which to add the next char, represents the length.
- * @str: String to print
- * @precision: Maximum number of characters to print
- * Return: Number of characters printed
- */
-int print_string(char buffer[], int *buff_ind, const char *str, int precision)
-{
-    (void)buffer;    // Unused parameter
-    (void)buff_ind;  // Unused parameter
-    (void)str;       // Unused parameter
-    (void)precision; // Unused parameter
+    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+    {
+        if (is_digit(format[curr_i]))
+        {
+            precision *= 10;
+            precision += format[curr_i] - '0';
+        }
+        else if (format[curr_i] == '*')
+        {
+            curr_i++;
+            precision = va_arg(list, int);
+            if (precision < 0)
+            {
+                /* Handle negative precision as if it was not specified */
+                precision = -1;
+            }
+            break;
+        }
+        else
+        {
+            break;
+        }
+    }
 
-    // Your existing implementation for printing a string
+    *i = curr_i - 1;
 
-    return 0; // Modify according to your logic
+    return (precision);
 }

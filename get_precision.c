b127cdd0b2afadba751365088ece5dac908f3144
Omar J <omarj819@gmail.com>
2023-11-29 01:05:29 +0200

    A

--- get_precision.c
+++ get_precision.c
@@ -1,60 +1,25 @@
 #include "main.h"
 
-/**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: Index in the format string
- * @list: List of arguments
- *
- * Return: Precision.
- */
-int get_precision(const char *format, int *i, va_list list)
+int is_digit(char c)
 {
-    int curr_i = *i + 1;
-    int precision = -1;
-
-    if (format[curr_i] != '.')
-        return (precision);
-
-    precision = 0;
-
-    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-    {
-        if (is_digit(format[curr_i]))
-        {
-            precision *= 10;
-            precision += format[curr_i] - '0';
-        }
-        else if (format[curr_i] == '*')
-        {
-            curr_i++;
-            precision = va_arg(list, int);
-            if (precision < 0)
-            {
-                /* Handle negative precision as if it was not specified */
-                precision = -1;
-            }
-            break;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    *i = curr_i - 1;
-
-    return (precision);
+    return (c >= '0' && c <= '9');
 }
 
-/**
- * print_string - Print a string to the buffer with precision
- * @buffer: Buffer to print to
- * @buff_ind: Index at which to add next char, represents the length.
- * @str: String to print
- * @precision: Maximum number of characters to print
- * Return: Number of characters printed
- */
+void print_buffer(char buffer[], int *buff_ind)
+{
+    if (*buff_ind > 0)
+    {
+        write(1, buffer, *buff_ind);
+    }
+
+    *buff_ind = 0;
+}
+
+int get_precision(const char *format, int *i, va_list list)
+{
+    /* Your existing implementation for get_precision */
+} 
+
 int print_string(char buffer[], int *buff_ind, const char *str, int precision)
 {
     int i;
@@ -64,8 +29,10 @@ int print_string(char buffer[], int *buff_ind, const char *str, int precision)
         buffer[*buff_ind] = str[i];
         (*buff_ind)++;
         if (*buff_ind == BUFF_SIZE)
+        {
             print_buffer(buffer, buff_ind);
+        }
     }
 
-    return i; // Return the number of characters printed
+    return i; /* Return the number of characters printed */
 }
